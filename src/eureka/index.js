a719ee545cd88e84116cffcb7c22125f95e072a7
迷子 (Maiko Tan) <maiko.tan.coding@gmail.com>
2021-11-13 08:28:43 +0800

    Feat: support more cactbot modules (#4)
    * feat: add entries for other modules
    
    * fix: add entries in webpack.config.js
    
    * chore: add `yarn run watch` command

---
+++ src/eureka/index.js
@@ -0,0 +1,14 @@
+/* 
+Options.FlagTimeoutMs = 90;
+Options.CompleteNamesSTQ = false;
+Options.EnrichedSTQ = false;
+Options.PopNoiseForNM = true;
+Options.PopNoiseForBunny = true;
+Options.PopNoiseForSkirmish = false;
+Options.PopNoiseForCriticalEngagement = true;
+Options.PopNoiseForDuel = false;
+Options.PopVolume = 1;
+Options.BunnyPopVolume = 0.3;
+Options.CriticalPopVolume = 0.3;
+Options.RefreshRateMs = 1000;
+ */
